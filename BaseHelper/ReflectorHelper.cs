bc8708a64c8ac1ea0b70a25f53e610a96e2414c8
hukuolin@outlook.com <hukuolin@outlook.com>
2017-10-31 10:32:36 +0800

    2017-10-31  代码练手  -xml模板读取 ，数据填充 ，填充项的格式要求 xml内设置 {填充项的名称}

---
+++ BaseHelper/ReflectorHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+namespace BaseHelper
+{
+    public static class ReflectorHelper
+    {
+        /// <summary>
+        /// 获取实体类的全部属性
+        /// </summary>
+        /// <typeparam name="T">实体类</typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string[] GetProperties<T>(this T obj) where T:class
+        {
+           Type t= typeof(T).GetType();
+           string[] pns= t.GetProperties().Select(s => s.Name).ToArray();
+           return pns;
+        }
+        /// <summary>
+        /// 根据 提供的属性名获取该属性的值内容的字符串
+        /// </summary>
+        /// <typeparam name="T">实体类型</typeparam>
+        /// <param name="obj">具体的实体</param>
+        /// <param name="propertyName">属性名</param>
+        /// <returns>字符串</returns>
+        public static string GetPropertyValueString<T>(this T obj,string propertyName) where T : class
+        {
+            Type t= typeof(T).GetType();
+            PropertyInfo pi= t.GetProperty(propertyName);
+            //此处需要判断该该属性的数据类型是否对应class
+            Type pt = pi.PropertyType;//该属性的数据类型
+            if (pt.IsPrimitive) 
+            {//是否为基源数据类型
+            
+            }
+            string value = string.Empty;
+            object pv= pi.GetValue(obj, null);
+            if (pv == null) 
+            {
+                return null;
+            }
+            return pv.ToString();
+        }
+    }
+}
---
+++ BaseHelper/XmlHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+namespace BaseHelper
+{
+    public static class XmlHelper
+    {
+        /// <summary>
+        /// 从指定xml文件中读取，并将数据填写到模板中【 模板填充格式 {config}】
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="xmlPath"></param>
+        /// <param name="waitFillData">填充的数据</param>
+        /// <returns></returns>
+        public static string XmlTemplateFillData<T>(this string xmlPath,T waitFillData) where T:class 
+        {
+            string xml = ReadXml(xmlPath);//读取xml模板文件内容
+            string[] pv = waitFillData.GetProperties();
+            foreach (var item in pv)
+            {
+               string value=  waitFillData.GetPropertyValueString(item);
+               xml = xml.Replace(string.Format("{0}", item), value);
+            }
+            return xml;
+        }
+        static string  ReadXml(string xmlPath)
+        {
+            string text = string.Empty;
+            if (!File.Exists(xmlPath)) 
+            {//没有找到该xml文件
+                return text;
+            }
+            FileStream fs = new FileStream(xmlPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            MemoryStream ms = new MemoryStream();
+            fs.Seek(0, SeekOrigin.Begin);
+            StreamReader sr = new StreamReader(fs);
+            sr.Dispose();
+            fs.Dispose();
+            return sr.ReadLine();
+        } 
+
+    }
+}
