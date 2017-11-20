c6248f9c92e361306e754a6436b7242c4daf67c5
hukuolin@outlook.com <hukuolin@outlook.com>
2017-11-20 13:39:28 +0800

    2017-11-20 漏提交文件检测4

---
+++ DataProvider.Service/DataProvider.IService/IAsyncDataService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProvider.IService
+{
+    public interface IAsyncDataService<T>
+    {
+        string SqlConnString { get; }
+        int Count(string cmd);
+        int ExecuteNoQuery(string cmd);
+    }
+}
