0e6416d011f47e3430ae61a1cccdb85a4ced0fb2
hukuolin@outlook.com <hukuolin@outlook.com>
2017-11-20 13:31:38 +0800

    2017-11-20 漏提交文件检查1

---
+++ DayDayStudyWin/ImageClickCodeFrm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace DayDayStudyWin
+{
+    partial class ImageClickCodeFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbMouse = new System.Windows.Forms.RichTextBox();
+            this.pbCodeImg = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCodeImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rtbMouse
+            // 
+            this.rtbMouse.Location = new System.Drawing.Point(311, 12);
+            this.rtbMouse.Name = "rtbMouse";
+            this.rtbMouse.Size = new System.Drawing.Size(215, 311);
+            this.rtbMouse.TabIndex = 8;
+            this.rtbMouse.Text = "";
+            // 
+            // pbCodeImg
+            // 
+            this.pbCodeImg.Location = new System.Drawing.Point(13, 13);
+            this.pbCodeImg.Name = "pbCodeImg";
+            this.pbCodeImg.Size = new System.Drawing.Size(292, 310);
+            this.pbCodeImg.TabIndex = 9;
+            this.pbCodeImg.TabStop = false;
+            // 
+            // ImageClickCodeFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 334);
+            this.Controls.Add(this.pbCodeImg);
+            this.Controls.Add(this.rtbMouse);
+            this.Name = "ImageClickCodeFrm";
+            this.Text = "ImageClickCodeFrm";
+            ((System.ComponentModel.ISupportInitialize)(this.pbCodeImg)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbMouse;
+        private System.Windows.Forms.PictureBox pbCodeImg;
+    }
+}
---
+++ DayDayStudyWin/ImageClickCodeFrm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DayDayStudyWin
+{
+    public partial class ImageClickCodeFrm : Form
+    {
+        public ImageClickCodeFrm()
+        {
+            InitializeComponent();
+            InitPicture();
+        }
+        void InitPicture() 
+        {
+            pbCodeImg.PictureBoxImage(AppConfig.CodeImg12306Dir);
+            //生成小图标
+            PictureBox icon = new PictureBox();
+            icon.Size = new Size(22, 22);
+            icon.PictureBoxImage(AppConfig.IConDir);
+        }
+    }
+}
