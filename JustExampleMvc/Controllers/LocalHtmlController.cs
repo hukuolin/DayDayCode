5468308b2ed7bb41bd715fbcf949e79b40840920
hukuolin@outlook.com <hukuolin@outlook.com>
2017-11-20 13:43:01 +0800

    2017-11-20 漏提交文件检测5

---
+++ JustExampleMvc/Controllers/LocalHtmlController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JustExampleMvc.Controllers
+{
+    public class LocalHtmlController : Controller
+    {
+        //
+        // GET: /LocalHtml/
+
+        public ActionResult JsNewDate()
+        {
+            return View();
+        }
+    }
+}
